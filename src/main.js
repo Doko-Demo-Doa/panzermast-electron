0d904ce6613335038b2a7fc815dce800ffa653a4
Quan Pham <doraemonfanclub@gmail.com>
2019-03-18 10:18:26 +0700

    Added AntDesign Vue

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from "./app.vue";
 import router from "./router";
 import store from "./store";
 
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+
+Vue.use(Antd);
+
 Vue.config.productionTip = false;
 
 new Vue({
